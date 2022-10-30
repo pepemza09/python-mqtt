eb31045d11e9e7e9634420d4f7a78e517d58b9ad
pepemza09 <victor.costa08@gmail.com>
2022-10-30 18:57:04 -0300

    completado el programa subcriptor

--- mqttsub.py
+++ mqttsub.py
@@ -4,8 +4,13 @@ from termcolor import colored
 from threading import Thread
 import emoji
 
-def ante_conexion_exitosa(client, userdata, flag, rc):
-    print("Conectado con el broker" + emoji.emojize(':grinning_face:'))
+def ante_con_exitosa(client, userdata, flag, rc):
+    print('Conectado con el servidor')
+    client.subscribe('/chat/mensaje')
+    
+
+def ante_llegada_mensaje(client, userdata, msg):
+    print('llegó el siguiente mensaje: ' + msg.payload.decode("utf-8")) #se imprime el mensaje que viene en el tópico
 
 print(colored("Bienvenidos al cliente de mosquitto" + emoji.emojize(':thumbs_up:'),"red"))
 nombre = input("Primero, escribe tu nombre: ")
@@ -19,6 +24,15 @@ else:
     servidor = input('Ingresa el nombre/IP del servidor MQTT: ')
     usuario = input('Ingresa el usuario: ')
     clave = input('Ingresa la clave: ')
+    #incializamos el cliente
+    cliente = mqtt.Client()
+    #nos conectamos al tópico
+    cliente.on_connect = ante_con_exitosa
+    cliente.username_pw_set(usuario,clave)
+    cliente.connect(servidor,1883,60)
+    while True:
+        cliente.on_message = ante_llegada_mensaje #imprimimos el mensaje que llega
+        cliente.loop()
 
 
 '''
